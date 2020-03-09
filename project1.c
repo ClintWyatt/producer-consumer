49506b67ce3d28afb9bddbf7611ae2d50220940d
Clint Wyatt <ClintWyatt@my.unt.edu>
2020-03-09 14:49:04 -0500

    adding the linked list functionality to freelist

--- project1.c
+++ project1.c
@@ -2,6 +2,7 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <time.h>
 
 void *thread1();
 void *thread2();
@@ -17,15 +18,41 @@ struct listType
 
 int main()
 {
-	struct listType *freeList = (struct listType *)malloc(sizeof(struct listType));//allocating memory for the free list
-	struct listType *list1 = (struct listType *)malloc(sizeof(struct listType));//allocating memory for list1
-	struct listType *list2 = (struct listType *)malloc(sizeof(struct listType));//allocating memory for list2
+	srand(time(NULL));
+	//struct listType *freeList = (struct listType *)malloc(sizeof(struct listType));//allocating memory for the free list
+	//struct listType *list1 = (struct listType *)malloc(sizeof(struct listType));//allocating memory for list1
+	//struct listType *list2 = (struct listType *)malloc(sizeof(struct listType));//allocating memory for list2
 	
 	//heads to the linked lists
-	struct listType *t1Head = NULL;
-	struct listType *t2Head = NULL;
-	struct listType *t3Head = NULL;
+	struct listType *freeHead = NULL;
+	struct listType *list1Head = NULL;
+	struct listType *list2Head = NULL;
+	struct listType *current = NULL; //used for deleting and adding nodes
+	int i; //
+	int counter =0; //used for the number of loops required for the program
 
+
+	//creating the linked list for the free list to 5 nodes
+	for(i =0; i < 8; i++)
+	{
+		if(freeHead == NULL)
+		{
+			struct listType *node = (struct listType *) malloc(sizeof(struct listType));//creating the first node
+			node->next = NULL;
+			freeHead = node;
+			freeHead->data = rand() % 100;//giving the node a randome number 
+		}
+		else
+		{
+			struct listType *node = (struct listType *) malloc(sizeof(struct listType));//creating the first node
+			node->next = NULL;
+			node->next = freeHead;
+			freeHead->data = rand() % 100;
+		}
+	}
+
+
+	//creating the threads
 	pthread_create(&t1, NULL, (void *) &thread1, NULL);
 	pthread_create(&t2, NULL, (void *) &thread2, NULL);
 	pthread_create(&t2, NULL, (void *) &thread3, NULL);
@@ -35,8 +62,35 @@ int main()
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
 	pthread_join(t3, NULL);
+	
 
-	//free memory before closing the program
+	//free memory from free list
+	current = freeHead;
+	while(current != NULL)
+	{
+		current = current->next;
+		free(freeHead);//deelting alloacted memory
+		freeHead = current;
+	}
+
+	//free memory from list1
+	current = list1Head;
+	while(current != NULL)
+	{
+		current = current->next;
+		free(list1Head);
+		list1Head = current;
+	}
+	
+	//free memory from list2
+	current = list2Head;
+	while(current != NULL)
+	{
+		current = current->next;
+		free(list2Head);
+		list2Head = current;
+	}
+	
 	return 0;
 }
 
