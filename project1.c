28b0d71a78fb63c4fc4ec6ed19cfb97c84298110
Clint Wyatt <ClintWyatt@my.unt.edu>
2020-03-10 18:04:36 -0500

    adding semaphores and partial thread functionality

--- project1.c
+++ project1.c
@@ -8,7 +8,7 @@ void *thread1();
 void *thread2();
 void *thread3();
 pthread_t t1, t2, t3;//threads 
-sem_t sem1, sem2, sem3, mutualExclusion; //semaphores
+sem_t freeList, list1, list2, mx, freeBlock; //semaphores
 struct listType
 {
 	struct listType *next;//head of the linked list free list
@@ -28,9 +28,9 @@ int main()
 	struct listType *list1Head = NULL;
 	struct listType *list2Head = NULL;
 	struct listType *current = NULL; //used for deleting and adding nodes
-	int i; //
+	int i; //shared variable for looping
 	int counter =0; //used for the number of loops required for the program
-
+	int limit = 250;
 
 	//creating the linked list for the free list to 5 nodes
 	for(i =0; i < 8; i++)
@@ -50,8 +50,13 @@ int main()
 			freeHead->data = rand() % 100;
 		}
 	}
-
-
+	
+	sem_init(&freeList, 0, 8);//initializing the counting semaphore for the free list
+       	sem_init(&list1, 0, 0);//initializing the counting semaphore for list1
+	sem_init(&list2, 0, 0)l //initializing the counting semaphore for list2
+	sem_init(&mx, 0, 1);//initializing the binary semaphore for mutual exclusion
+	sem_init(&freeBlock, 0, 0); //initializing the binary semaphore freeblcok, which is used when the free list has 1 node
+		
 	//creating the threads
 	pthread_create(&t1, NULL, (void *) &thread1, NULL);
 	pthread_create(&t2, NULL, (void *) &thread2, NULL);
@@ -96,24 +101,40 @@ int main()
 
 void *thread1()
 {
+	while(counter < limit)
+	{
+		sem_wait(&freeList);
+		sem_wait(&mx);
+		//add to list 1 and take away from free list
+		
 
-
-
+		sem_post(&list1);//element has been added to list1. 
+		sem_post(&mx);//put any of the threads that are blocked on the ready queue
+		counter ++;
+	}
 
 }
 
 void *thread2()
 {
+	while(counter < limit)
+	{
+		sem_wait(&list1);
+		sem_wait(&mx);
+		//take 
 
-
+	}
 
 }
 
 void *thread3()
 {
+	while(counter < limit)
+	{
 
 
 
+	}
 
 }
 
