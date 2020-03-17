ce6a7d6e1ebe4251b4eb133eba7ce04a461ec6e4
isaacthomas344 <isaacthomas344@gmail.com>
2020-03-16 20:56:19 -0500

    Added thread 3 code and modified thread 2

--- project1.c
+++ project1.c
@@ -116,7 +116,7 @@ void *thread1()
                 printf("Nodes in the free list: ");
                 while(current != NULL)
                 {
-			current->data = rand() %100;//randomizing the data for the node
+						current->data = rand() %100;//randomizing the data for the node
                         printf("%d -> ", current->data);//printing the data in the free list
                         current = current->next;//advancing to the next node
                 }
@@ -132,7 +132,7 @@ void *thread1()
                 {
                         trailPtr = freeHead;//setting the trailPtr to the free list's head
                         freeHead = freeHead->next;//advancing the free list to the next node
-                        trailPtr->next = list1Head;//having the origional freelist head point to the head of list1
+                        trailPtr->next = list1Head;//having the original freelist head point to the head of list1
                         list1Head = trailPtr;//resetting the head for list1
                 }
                 trailPtr = NULL;
@@ -151,41 +151,49 @@ void *thread2()
 	{
 		sem_wait(&list1);
 		sem_wait(&mx);
-		//getting the node from list 1 
+
+		//unlink node from list 1 
 		list1Ptr = list1Head;
 		list1Head = list1Ptr->next;
+		list1Ptr->next = NULL;
 
-		//getting the node from the free list
+		//unlink node from the free list
 		freelistPtr = freeHead;
 		freeHead = freelistPtr->next;
+		freelistPtr->next = NULL;
 
-		//adding node from list1 to the freelist
-		if(list1Head == NULL) //check if inital list is empty
+		//Use list1Ptr(x) to produce freelistPtr(y) data
+		printf("Data before transfer:\n");
+		printf("Node X: %d\tNode Y: %d\n", list1Ptr->data, freelistPtr->data);
+		freelistPtr->data = list1Ptr->data;
+
+
+		//link node from list1 to the freelist
+		if(freeHead == NULL) //check if inital list is empty
 		{
-			list1Head = list1Ptr;
-			list1Ptr = NULL;
+			freeHead = list1Ptr;
 		}
 		else
 		{
 			list1Ptr->next = freeHead; //reset freelist head to new node from list 1
 			freeHead = list1Ptr; //Set the new freelist head
-			list1Ptr = NULL; //set un-needed pointer to NULL
 		}
 		
 
-		//adding node from the freelist to list2
+		//link node from the freelist to list2
 		if(list2Head == NULL) //check if inital list is empty
 		{
 			list2Head = freelistPtr;
-			freelistPtr = NULL;
 		}
 		else
 		{
 			freelistPtr->next = list2Head; //reset list 2 head to new node from freelist 
 			list2Head = freelistPtr; //Set the new list 2 head
-			freelistPtr = NULL; //set un-needed pointer to NULL
 		}
 		
+		//set un-needed pointer to NULL
+		list1Ptr = NULL;
+		freelistPtr = NULL;
 
 		counter++;
 		sem_post(&list2);//node has been added to list2
@@ -197,13 +205,37 @@ void *thread2()
 
 void *thread3()
 {
+	struct listType *list2Ptr = NULL;
+	struct listType *freelistPtr = NULL;
+
 	while(counter < limit)
 	{
 		sem_wait(&list2);
 		sem_wait(&mx);
 
 		//take node from list2 and add it to the free list
+
+		//unlink node from list 2
+		list2Ptr = list2Head;
+		list2Head = list2Ptr->next;
+		list2Ptr->next = NULL;
+
+		//comsume list2Ptr info by setting to zero
+		list2Ptr->data = 0;
+
+		//Links list 2 node to freelist
+		if(freeHead == NULL) //Checks if list is empty
+		{
+			freeHead = list2Ptr;
+		}
+		else
+		{
+			list2Ptr->next = freeHead;
+			freeHead = list2Ptr;
+		}
 		
+		//set un-needed pointer to NULL
+		list2Ptr = NULL;
 		
 		counter++;
 		sem_post(&freeList); //node has been added to the free list
